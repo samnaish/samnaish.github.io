294c4497947ba5a54ded7a0ce5f1337ca02fdd41
Sam Naish <sam.naish92@googlemail.com>
2024-10-30 12:13:16 +0000

    made more componets

---
+++ src/componets/footer/index.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+    <section className="section" id="profiles">
+      <h2 className="section__heading">My Profiles</h2>
+      <p className="section__content">
+        <ul className="section__contacts">
+          <li><a href="https://github.com/samnaish" target="_blank" className="section__contact-detail">GitHub</a></li>
+          <li><a href="https://codepen.io/samnaish/" target="_blank" className="section__contact-detail">Codepen</a></li>
+        </ul>
+      </p>
+    </section>
+    )
+}
