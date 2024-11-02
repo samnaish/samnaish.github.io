720f4ca6c80032c1d9f2e70fc502ee7eced6293e
Sam Naish <sam.naish92@gmail.com>
2024-11-02 16:25:09 +0000

    fix: made app responsive

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -2,10 +2,15 @@ import { Social } from "../social";
 
 const NavigationBar = () => {
     return (
-        <nav className="bg-stone-900 h-screen pt-8 flex flex-col text-center justify-between ">
+        <nav className="bg-stone-900 h-screen pt-8 flex flex-col text-center justify-between">
             <div>
-                <a className="block py-2 m-2 text-white rounded transition hover:bg-emerald-300 hover:text-stone-900" href="#top">Me</a>
-                <a className="block py-2 m-2 text-white rounded transition hover:bg-stone-200 hover:text-stone-900" href="#projects">Projects</a>
+                <a className="block py-2 m-2 text-white rounded transition hover:bg-emerald-300 hover:text-stone-900" href="#top">
+                    Me
+                </a>
+                <a className="block py-2 m-2 text-white rounded transition hover:bg-stone-200 hover:text-stone-900" href="#projects">
+                    <span className="md:hidden">Proj</span>
+                    <span className="hidden md:block">Projects</span>
+                </a>
             </div>
             <div>
                 <Social />
--- src/components/social/index.tsx
+++ src/components/social/index.tsx
@@ -4,8 +4,8 @@ const Social = () => {
   return (
     <ul className="">
       <li>
-        <a href="https://github.com/samnaish" target="_blank" className="inline-block mx-2 fill-white transition hover:fill-stone-900 hover:bg-white p-4 rounded">
-          <GithubIcon />
+        <a href="https://github.com/samnaish" target="_blank" className="inline-block mx-2 fill-white transition hover:fill-stone-900 hover:bg-white md:p-4 rounded">
+          <GithubIcon className="size-12 md:size-16" />
         </a>
       </li>
     </ul>
